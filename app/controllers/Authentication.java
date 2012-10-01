c4f10e477f43566e15ee4266f7dc55bcb6d95754
Ivar Trygve Jarlsby <it.jarlsby@stud.uis.no>
2012-10-01 08:33:45 +0200

    Further work on user system

--- app/controllers/Authentication.java
+++ app/controllers/Authentication.java
@@ -4,25 +4,66 @@ import views.html.user.*;
 import models.User;
 import play.mvc.*;
 
+import play.data.*;
+import play.data.validation.Constraints.*;
+
 public class Authentication extends Controller {
 
+	final static Form<RegisterData> registerForm = form(RegisterData.class);
+
 	public static Result registerForm() {
-		return ok(register.render());
+		return ok(register.render(registerForm));
 	}
 
 	public static class RegisterData {
+		@Required
+		@MinLength(1)
 		public String username;
+
+		@Required
+		@MinLength(7)
 		public String password;
 		public String password_repeat;
+
+		@Required
+		@Email
 		public String email;
+
+		@Required
+		@MinLength(1)
 		public String fullname;
 	}
 
 	public static Result register() {
-		RegisterData form = form(RegisterData.class).bindFromRequest().get();
+		Form<RegisterData> filledForm = registerForm.bindFromRequest();
+		if(filledForm.hasErrors()) {
+			return ok(register.render(filledForm));
+		}
+
+		RegisterData data = filledForm.get();
+
+		// Check if username is already taken
+		if(filledForm.error("username") == null) {
+			User samename;
+			try {
+				samename = User.getByUsername(data.username);
+			} catch (Exception e) {
+				return ok("Unable to check whether your username is taken.");
+			}
+			if(samename != null)
+				filledForm.reject("username", "Username already taken.");
+		}
+
+		// Check if the two passwords match
+		if(filledForm.error("password") == null && data.password != data.password_repeat)
+			filledForm.reject("password_repeat", "Password does not match.");
+
+		if(filledForm.hasErrors()) {
+			return ok(register.render(filledForm));
+		}
 
 		try {
-			new User(form.username, form.password, form.email, form.fullname);
+			new User(data.username, data.password, data.email, data.fullname);
 			return ok("Registration succeded!");
 		} catch (Exception e) {
 			return ok("Database error");
@@ -34,6 +75,7 @@ public class Authentication extends Controller {
 	}
 
 	public static class LoginData {
+		@Required
 		public String username;
 		public String password;
 	}
--- app/models/User.java
+++ app/models/User.java
@@ -7,13 +7,14 @@ import org.mindrot.BCrypt;
 public class User {
 
 	private String username;
-	private String passhash;
 	private String email;
 	private String fullname;
 
+	private User() {}
+
 	public User(String username, String password, String email, String fullname) throws Exception {
 		this.username = username;
-		this.passhash = BCrypt.hashpw(password, BCrypt.gensalt(12));
+		String passhash = BCrypt.hashpw(password, BCrypt.gensalt(12));
 		this.email = email;
 		this.fullname = fullname;
 
@@ -42,4 +43,18 @@ public class User {
 		return BCrypt.checkpw(password, passhash);
 	}
 
+	// Returns null when there is no user by the username
+	public static User getByUsername(String username) throws Exception {
+		Connection conn = DB.getConnection();
+		PreparedStatement stmt = conn.prepareStatement("select email, fullname from user where username = ?"); 
+		stmt.setString(1, username);
+		ResultSet result = stmt.executeQuery();
+		if(result.next() == false)
+			return null;
+		User retval = new User();
+		retval.username = username;
+		retval.email = result.getString(1);
+		retval.fullname = result.getString(2);
+		return retval;
+	}
 }
