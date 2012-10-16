65e36a96e14368a9565f5a910333dd4654bfb1f6
ralbraji <roya_525@hotmail.com>
2012-10-16 10:58:23 +0200

    forsiden

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,13 +2,26 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.*;
+
+import models.*;
 
 import views.html.*;
 
 public class Application extends Controller {
-
-	public static Result index() {
-		return ok(index.render("Your new application is ready."));
-	}
-
+  
+    static Form<Login> taskForm = form(Login.class);
+  public static Result index() {
+      return ok(views.html.index.render(Login.all(), taskForm)
+      );
+  }
+    
+    public static Result login(){
+        return TODO;
+    }
+    
+    public static Result registrer(){
+        return TODO;
+    }
+  
 }
---
+++ app/models/Login.java
@@ -0,0 +1,21 @@
+package models;
+
+
+import java.util.*;
+
+public class Login {
+    
+    public String brukernavn;
+    public String passord;
+  
+  public static List<Login> all() {
+      return new ArrayList<Login>();
+  }
+    
+    public static void login(){
+    }
+    
+    public static void registrer(){
+    }
+  
+}
