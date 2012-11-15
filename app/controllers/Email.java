49ebb770be1b3a4d714abef2749d359e531ab7d9
stinejf <stinejf@gmail.com>
2012-11-15 10:25:23 +0100

    Added a redirect if the user is not logged in and is trying to send an email.
    In send(); From is now "Minerva" and added an email-string so that the
    user can reply.

--- app/controllers/Email.java
+++ app/controllers/Email.java
@@ -5,8 +5,7 @@ import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
-import views.html.email;
-import views.html.sendemail;
+import play.Play;
 
 import com.typesafe.plugin.*;
 
@@ -15,31 +14,35 @@ public class Email extends Controller{
 	private static final Form<Emails> emailForm = form(Emails.class);
 
 	public static Result create(Long id) {
-		User created = new User(); 
-		created.id = id;
-		return ok(email.render(emailForm, created));
+		User currentUser = Authenticator.getCurrentUser(); 
+		if (currentUser == null) {
+			return redirect("/");
+		}
+		else {
+			User created = new User(); 
+			created.id = id;
+			return ok(email.render(emailForm, created));
+		}
 	}
 
 	public static Result send(Long userID) {
 		Form<Emails> email = emailForm.bindFromRequest();  
-		
 		User currentUser = Authenticator.getCurrentUser(); 
 		User toUser = User.getByUserId(userID); 
 		
 		Emails currentEmail = email.get();
 	
 		String fullName = currentUser.fullName; 
-		String from = currentUser.email;
+		String from = currentUser.email; 
 		String recipient = toUser.email;
 		String subject = currentEmail.subject; 
-		String text = currentEmail.text; 
-
-		MailerAPI mail = play.Play.application().plugin(MailerPlugin.class).email();
+		String text = currentEmail.text;
 		
+		MailerAPI mail = play.Play.application().plugin(MailerPlugin.class).email();	
 		mail.setSubject(subject);
 		mail.addRecipient(recipient);
-		mail.addFrom(from);
-		mail.send(fullName + " " +Language.get("Wrote") + ":\n\n"+text);
-		return ok(sendemail.render(currentEmail));
+		mail.addFrom("Minerva <"+from+">");
+		mail.send(Language.get("ReceivedMessage")+"\n\n"+fullName + " " +Language.get("Wrote") + ":\n\n"+text+"\n\n\n"+Language.get("Reply")+": "+currentUser.email);
+		return ok(emailSent.render(currentEmail));
 	}
 }
